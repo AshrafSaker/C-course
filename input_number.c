42871d6fe7fbe2175fb07fc76c9badd53e6ab677
Ashraf Saker <engashrafsaker@gmail.com>
2024-08-21 23:07:30 +0300

    input number

---
+++ input_number.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(){
+    
+
+        //input Number
+    printf("****Input number*****\n");
+    int a;
+    printf("Write : ");
+    scanf("%d",&a); //input stings
+    printf("You entered: %d \n",a);
+
+
+
+    return 0;
+}
