e0baf9406fb658b028e023673007ce597f916242
Ashraf Saker <engashrafsaker@gmail.com>
2024-08-21 23:59:33 +0300

    loop to not close

--- input_strings.c
+++ input_strings.c
@@ -14,14 +14,22 @@ int main(){
     char choice;
 
     while (1) {
-        printf("اختر (q للخروج): ");
+        printf("press y to Exit ");
         scanf(" %c", &choice);
 
-        if (choice == 'q') {
+        if (choice == 'y') {
             break; // الخروج من الحلقة
         }
 
         // باقي الكود الخاص بك
+            printf("****Input Strings*****\n");
+    char st[0]; //to store char in string
+    printf("Write string: ");
+    gets(st); //input stings
+    printf("You entered: %s \n",st);
+    //output strings
+    puts(st);
+    char choice;
     }
 
 
