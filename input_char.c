14e6905b25823ff8ca663284348f58b1668e8dda
Ashraf Saker <engashrafsaker@gmail.com>
2024-08-21 23:02:33 +0300

    input strings

--- input_char.c
+++ input_char.c
@@ -9,12 +9,7 @@ int main(){
     //output print
     printf("You enter char: %c \n",a);
     
-    //input Stings
-    printf("****Input Strings*****\n");
-    char st[0]; //to store char in string
-    printf("Write string: \n");
-    gets(st); //input stings
-    printf("You entered: %s \n",st);
+
 
 
     return 0;
---
+++ input_strings.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(){
+    
+
+        //input Stings
+    printf("****Input Strings*****\n");
+    char st[0]; //to store char in string
+    printf("Write string: ");
+    gets(st); //input stings
+    printf("You entered: %s \n",st);
+
+
+
+    return 0;
+}
