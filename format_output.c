4f0885a042e4eb2a95d3ffe44251e0643b090999
Ashraf Saker <engashrafsaker@gmail.com>
2024-08-22 00:52:45 +0300

    format output

---
+++ format_output.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+
+int main(){
+    printf("The %s has %d %s","Tree",22,"apples");
+    //output:  The Tree has 22 apples
+    //نقدر نعوض عن كل مدخل بعد العلامة % وتحديد نوعها بفاصل "," زي المثال
+
+    return 0;
+}
+
+
+
+
+/* 
+Formatting Output 
+
+Escape sequences begin with a backslash \:
+
+\n new line
+
+\t horizontal tab
+
+\\ backslash
+
+\b backspace
+
+\' single quote
+
+\" double quote
+
+indicated type:
+
+d decimal
+
+c character
+
+s string
+
+f float
+
+e scientific notation
+
+x hexadecimal
+
+
+*/
