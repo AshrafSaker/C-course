c83d54aa6a8a9e8192a392de60b58d51cfce39fa
Ashraf Saker <engashrafsaker@gmail.com>
2024-08-21 21:53:09 +0300

    Variables

--- datatype.c
+++ datatype.c
@@ -12,6 +12,26 @@ char: single character.
     printf("int: %Id \n",sizeof(int));
     printf("float: %Id \n",sizeof(float));
     printf("double: %Id \n",sizeof(double));
-    printf("char: %Id \n",sizeof(char));
+    printf("char: %ld \n",sizeof(char));
+    
+
+/* المتغيرات*/
+
+    printf("----Variables ---- \n");
+
+int a , b ;
+float salary = 56.23;
+char letter = 'Z';
+a = 8;
+b = 34;
+
+int c = a + b;
+
+printf("%d \n",c);
+printf("%f \n",salary);
+printf("%c \n",letter);
+
+
+
     return 0;
 }
