6589eea4cc27059afe08f6076a3479e800e891d2
Ashraf Saker <engashrafsaker@gmail.com>
2024-08-22 00:34:01 +0300

    try number Before %

--- format_input.c
+++ format_input.c
@@ -16,16 +16,30 @@ x hexadecimal
 
 */
 #include <stdio.h>
-int main(){
-    int x;
-    float num;
-    char text[20];
-
+    int main(){
+         int x;
+      float num;
+     char text[20];
+    printf("write dd fff sss\n");
     scanf("%2d %3f %3s",&x,&num,text); //use number لتحديد ادخال كل حرف في المتاولية
     //input 22 2.5 Ashrf
-    printf("int: %d\nfloat: %f\nText; %s\n",x,num,text);
-    /* 
+    /*   
+    if without number  %d %f %s
+    input 22235 2.5554 ashraf
+    int: 22235     
+    float: 2.555400
+    Text; ashraf
+
+    %2d %f %*s
+    write dd ffff sss
+    22 3.55 asd
     int: 22
+    float: 3.550000
+    Text:
+    */
+    printf("int: %d\nfloat: %f\nText:%s\n",x,num,text);
+    /*  
+    int: 22 
     float: 2.500000
     Text; Ash 
     */
