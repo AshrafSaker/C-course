3e4a000a7ca7a0e1f57835aac745ace5a5ecb192
Ashraf Saker <engashrafsaker@gmail.com>
2024-08-21 23:11:29 +0300

    input many scanf

---
+++ input_2number.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(){
+    
+
+        //input Number
+    printf("****Input 2number*****\n");
+    int a,b; // to make tow varibale numbe
+    printf("Write 2number : ");
+    scanf("%d %d",&a ,&b); //input for every input %d and ,
+    printf("Sum= %d \n",a+b); // sum a + b
+
+
+
+    return 0;
+}
