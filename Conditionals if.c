63426bac5479a87c6300e53ac535e927a96cf02b
Ashraf Saker <engashrafsaker@gmail.com>
2024-08-24 23:48:36 +0300

    Relational

--- Conditionals if.c
+++ Conditionals if.c
@@ -7,6 +7,16 @@ int main(){
         /* code */
         printf("\n \n PASS!!!!   \n \n");
     }
+
+    int num = 41;
+    num += 1;
+
+    if (/* condition */ num == 42)
+    {
+        /* code */
+        printf("\n you Win!!!! \n");
+    }
+    
     
     return 0;
 }
