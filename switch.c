f2f32e45ef53c107a8e885e25c7490957111d80c
Ashraf Saker <engashrafsaker@gmail.com>
2024-08-25 22:37:33 +0300

    multi switch

--- switch.c
+++ switch.c
@@ -22,4 +22,28 @@ int main(){
         printf("not Defention \n");
         break;
     }
+
+    //multi switch case
+
+    int x=8;
+    switch(x){
+        case 1:
+        case 2:
+        case 3:
+        printf("1 to 3 \n");
+        break;
+        case 4:
+        case 5:
+        case 6:
+        printf("4 to 6 \n");
+        break;
+        case 7:
+        case 8:
+        case 9:
+        printf("7 to 9 \n");
+        break;
+        default:
+        printf("Don't know!!!!");
+        break;
+    }
 }
