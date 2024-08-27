17427e1f3b6fd7bc9d4081931ed7376d5d9e9cfd
Ashraf Saker <engashrafsaker@gmail.com>
2024-08-27 19:51:02 +0300

    continue

--- while Loop.c
+++ while Loop.c
@@ -2,7 +2,7 @@
 
 int main(){
 
-    printf("While Loop \n");
+    printf("\nWhile Loop \n");
     int num = 1;
 
     while (num <= 3)
@@ -11,10 +11,40 @@ int main(){
         printf("Num: %d \n",num);
         num++;
     }
+
+    printf("\nDo-While-Loop \n");
     int n = 1;
     do {
         printf("N= %d \n",n);
         n++;
-    } while (n <= 3);
+    } while (n < 3);
+
+    printf("\nBreak mins While \n");
+
+    int m=10;
+
+    while (n>1)
+    {
+        if (m==7)
+        break;
+        printf("M= %d \n",m);
+        m--;
+
+    }
+
+    printf("\ncontinue\n");
+    int c=10;
+    while (c>1)
+    {
+        c--;
+        if(c>=3 && c<=7)
+        continue;
+        
+        printf("continue=%d\n",c);
+        
+    }
+    
+
+    
     return 0;
 }
