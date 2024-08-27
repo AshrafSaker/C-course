8f48cb02088c01b8ede04bcbec8089b06ee8d169
Ashraf Saker <engashrafsaker@gmail.com>
2024-08-27 18:58:52 +0300

    while

---
+++ while Loop.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(){
+
+    printf("While Loop \n");
+    int num = 1;
+
+    while (num <= 3)
+    {
+        /* code */
+        printf("Num: %d \n",num);
+        num++;
+    }
+    
+    return 0;
+}
