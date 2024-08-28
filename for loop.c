e64cdc170f7f9a954c289fbbae396c110dc50206
Ashraf Saker <engashrafsaker@gmail.com>
2024-08-28 23:47:36 +0300

    Multiplication table

--- for loop.c
+++ for loop.c
@@ -1,14 +1,34 @@
 #include <stdio.h>
 
 int main(){
+    /*
+    for (initvalue; condition; increment) {
+         statements;
+        }
+    */
     printf("\n FOR LOOP\n \n");
-    int i;
+    int s;
     int max = 10;
 
-    for ( i = 0; i < max; i++)
+    for ( s = 0; s < max; s++)
     {
         /* code */
-        printf("i= %d \n",i);
+        printf("S= %d \n",s);
     }
+
+    int i , j;
+    int table = 12;
+    int Max = 12;
+
+    for ( i = 0; i <= table; i++)
+    {
+        for ( j = 1; j <= Max; j++)
+        {
+          printf("%d x %d = %d \n",i,j,i*j);
+        }
+        printf("\n");
+        /* code */
+    }
+    
     
 }
