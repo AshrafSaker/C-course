539386afc6c322a53befea7312d25c5e5c568631
Ashraf Saker <engashrafsaker@gmail.com>
2024-08-28 21:48:30 +0300

    for loop

---
+++ for loop.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(){
+    printf("\n FOR LOOP\n \n");
+    int i;
+    int max = 10;
+
+    for ( i = 0; i < max; i++)
+    {
+        /* code */
+        printf("i= %d \n",i);
+    }
+    
+}
