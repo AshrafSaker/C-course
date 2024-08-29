caa868ae2f3eb9e528def29e0e3357535bd731d6
Ashraf Saker <engashrafsaker@gmail.com>
2024-08-29 23:44:54 +0300

    void and static

--- Functions.c
+++ Functions.c
@@ -1,20 +1,46 @@
 #include <stdio.h>
 int sq(int num); /* declaration */
 int sum(int a, int b);
+int global = 0;
+void sayhello();
 int main(){
-
+    //sq function
     int x,res;
     x= 5;
     res = sq(x);
 
     printf("%d Squared %d \n",x,res);
-
+    //sum with multi paramter function
     int f,s,re;
     f = 12;
     s = 3;
 
     re = sum(f,s);
     printf("%d + %d = %d \n",f,s,re);
+
+    // Varibal scope 
+
+    int local1,local2;
+
+    local1 = 28;
+    local2 = 42;
+
+    global = local1 + local2;
+
+    printf("Global =%d\n",global);
+
+    //Static & void function
+
+    int i;
+
+    for ( i = 0; i < 6; i++)
+    {
+        sayhello();
+    }
+    
+
+
+
 
 
 
@@ -32,4 +58,10 @@ int sq(int num){
 int sum(int a, int b){
     a += b;
     return a;
+}
+
+void sayhello(){
+    static int num = 1;
+    printf("Say Hello %d \n",num);
+    num++;
 }
