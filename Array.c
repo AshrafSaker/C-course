b7370903eba03cf4b395523afd4a6d93d17e6b7e
Ashraf Saker <engashrafsaker@gmail.com>
2024-08-31 23:03:40 +0300

    Loop in Array

--- Array.c
+++ Array.c
@@ -10,6 +10,22 @@ int main(){
     arr[1] = 300;
     printf("after change....!!!! \nIndex[1] = %d \n",arr[1]);
 
+    // LOOP in Array
+
+    printf("\n****Loop in Array *****\n");
+
+    int price[3]={10,5,30};
+    int total = 0;
+    int k;
+
+    for ( k = 0; k < 3; k++)
+    {
+        printf("Product (%d) = %dL.E \n",k+1,price[k]);
+        total += price[k];
+    }
+    printf("Total Price = %dL.E \n",total);
+    
+
 
 
 
