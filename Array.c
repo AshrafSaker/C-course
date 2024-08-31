252c69d0a64ad949a5bb5c945272d7caf81cbef9
Ashraf Saker <engashrafsaker@gmail.com>
2024-08-31 23:09:48 +0300

    iterates Loop

--- Array.c
+++ Array.c
@@ -25,6 +25,22 @@ int main(){
     }
     printf("Total Price = %dL.E \n",total);
     
+    // Loop iterates 
+     printf("\n**** iterates Loop  *****\n");
+    int a[5];
+    int i;
+
+    for ( i = 0; i < 5; i++)
+    {
+        a[i] = i*10;
+    }
+
+    for ( i = 0; i < 5; i++)
+    {
+        printf("iterates = %d \n",a[i]);
+    }
+    
+    
 
 
 
