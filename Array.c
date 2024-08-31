bd3f372800b80ce72ad0ecc159fdf29948d9a0ef
Ashraf Saker <engashrafsaker@gmail.com>
2024-08-31 22:55:00 +0300

    Array

---
+++ Array.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(){
+
+    //Array
+    printf("Array \n");
+
+    int arr[6]= {1,2,3,5,6,7};
+    printf("Index[1] = %d \n",arr[1]);
+    arr[1] = 300;
+    printf("after change....!!!! \nIndex[1] = %d \n",arr[1]);
+
+
+
+
+    return 0;
+}
