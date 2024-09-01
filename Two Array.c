03e910a56504064e10d71ae5bbc584484936ff5d
Ashraf Saker <engashrafsaker@gmail.com>
2024-09-01 23:16:12 +0300

    display 2 array

--- Two Array.c
+++ Two Array.c
@@ -7,10 +7,23 @@ int main(){
         {4,5,6},{7,8,9}
     };
 
-    printf("Value in array = %d\n",a[1][2]);
+    printf("Before Change = %d\n",a[1][2]);
     a[1][2]= 25;
-    printf("Value in array = %d\n",a[1][2]);
+    printf("After Change = %d\n\n",a[1][2]);
 
+    //Display Array 
+
+    int k,j;
+
+    for ( k = 0; k < 2; k++)
+    {
+        for ( j = 0; j < 3; j++)
+        {
+            printf("Value in Array %d \n",a[k][j]);
+        }
+        printf("\n");
+    }
+    
 
     return 0;
 }
