3e110b9ca664be609baab5e19c1ad1b626c8b50c
Ashraf Saker <engashrafsaker@gmail.com>
2024-09-02 23:43:21 +0300

    * ptr

--- Pointers .c
+++ Pointers .c
@@ -8,6 +8,16 @@ int main(){
     test(i);
     printf("The address of i= %x\n",&i);
     test(i);
+
+    // what is Pointer
+    printf("\n*********************\n");
+    int j=63;
+    int *p=NULL;
+    p = &j;
+    printf("The adress of j= %x\n",&j);
+    printf("p continer adress = %x\n",p);
+    printf("The Value of j = %d\n",j);
+    printf("P pointer value = %d\n",*p);
     return 0;
 }
 
