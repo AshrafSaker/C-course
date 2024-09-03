7c956ef63df2543398de0ba536c86c9207a5fc48
Ashraf Saker <engashrafsaker@gmail.com>
2024-09-03 23:44:56 +0300

    Function Swap

--- more pointer.c
+++ more pointer.c
@@ -1,4 +1,7 @@
 #include <stdio.h>
+
+void swap (int *num1, int *num2);
+
 int main(){
     int a[5]={20,22,36,45,48};
     int *ptr = NULL;
@@ -23,5 +26,21 @@ int main(){
     ptr ++;
     printf("%d  %x\n", *ptr, ptr);
     
+    printf("\n *************\n");
+    int x = 25;
+    int y = 100;
+
+    printf("x is %d, y is %d\n", x, y); 
+    swap(&x, &y);
+    printf("x is %d, y is %d\n", x, y); 
     return 0;
+}
+
+
+void swap (int *num1, int *num2) {
+    int temp;
+    
+    temp = *num1;
+    *num1 = *num2;
+    *num2 = temp;
 }
