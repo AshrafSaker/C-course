59de321f9c406bb306ba40297608dd7c386a25e9
Ashraf Saker <engashrafsaker@gmail.com>
2024-09-07 22:20:06 +0300

    String.h

--- string function.c
+++ string function.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <string.h> // library to sring strlen strcpy strcat
 
 
 int main(){
@@ -15,6 +16,18 @@ int main(){
 
     sscanf(info2,"%s %s %d",city,country,&pop);
     printf("The Popultion %s of %s country = %d \n",city,country,pop);
+    printf("\n**********************\n");
+    printf("\n********STRING.H*******\n");
+    printf("\n***********************\n");
+
+    char s1[ ]= "The gray fox ";
+    char s2[ ]= "Jumbed";
+    printf("Lenght S1 = %d \n",strlen(s1));
+    strcat(s1,s2);
+    printf("Add to S1+S2 = %s Length =%d \n",s1,strlen(s1));
+    strcpy(s1,s2);
+    printf("Strcpy S1 : S2 = %s Length = %d \n",s1,strlen(s1));
+
 
     return 0;
 }
